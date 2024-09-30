1108452548073b29133ee6afc74b8aed89cbac67
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-09-30 18:34:02 -0500

    feature/finByParameters:add some change

--- src/modules/books/interfaces/book-interface.interface.ts
+++ src/modules/books/interfaces/book-interface.interface.ts
@@ -1,6 +1,8 @@
 import {CreateBookDto} from '../dto/create-book.dto'
 import { FindById } from '../dto/find-by-Id.dto'
+import { UpdateBookDto } from '../dto/update-book.dto'
 import {Book} from '../schema/book.entity'
+
 
 
 export interface BookInterface {
@@ -8,9 +10,6 @@ export interface BookInterface {
     create(createBook: CreateBookDto):Promise<Book>
     findAll():Promise<Book[]>
     findOne(id:FindById):Promise<Book>
-
-
-
-
-
+    update(id:FindById, updateBook: UpdateBookDto):Promise<Book>
+    remove(id:FindById):Promise<void>
 }
